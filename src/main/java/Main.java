05e7752e2ea8f7bb8b26e93018806dd2b52e245d
Delta <christian.delta@gmx.at>
2019-11-28 12:14:46 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,82 @@
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        JSONObject Json = new JSONObject();
+        Json.put("firstName", "");
+        Json.put("lastName", "");
+        Json.put("email", "");
+        Json.put("challengeId", "");
+        Json.put("challengeAnswer", "");
+        Json.put("phone", "");
+        Json.put("cv", encodeFileToBase64Binary(""));
+        Json.put("messageToGepardec", "");
+        System.out.println(Json);
+
+        URL url = new URL ("https://weckdengeparden-57-services.cloud.itandtel.at/challenge/1/answer");
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "text/plain");
+        con.setDoOutput(true);
+
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = Json.toString().getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        }
+
+    }
+
+    private static String encodeFileToBase64Binary(String fileName) throws IOException {
+
+        File file = new File(fileName);
+        byte[] bytes = loadFile(file);
+        byte[] encoded = Base64.getEncoder().encode(bytes);
+        String encodedString = new String(encoded, StandardCharsets.UTF_8);
+
+        return encodedString;
+    }
+    private static byte[] loadFile(File file) throws IOException {
+        InputStream inputFileStream = new FileInputStream(file);
+
+        long length = file.length();
+        if (length > Integer.MAX_VALUE) {
+            System.out.println("File is to large.");
+        }
+        byte[] bytes = new byte[(int)length];
+
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length &&
+                (numRead=inputFileStream.read(bytes, offset, bytes.length-offset)) >= 0) {
+            offset += numRead;
+        }
+
+        if (offset < bytes.length) {
+            throw new IOException(String.format("File '%s' could not be read completely", file.getName()));
+        }
+
+        inputFileStream.close();
+        return bytes;
+    }
+
+}
